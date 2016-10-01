99c4034b5a08ef6584a0a9b01f600cea09497401
Adriann Sanchez <alka65@hotmail.com>
2016-10-01 14:30:15 -0500

    up

--- observatorio/settings.py
+++ observatorio/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'observatorio.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
-
+"""
 DATABASES = {
 	'default': {
 		'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -99,7 +99,7 @@ DATABASES = {
 		'HOST': '127.0.0.1',
 	}
 }
-"""
+
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
 
